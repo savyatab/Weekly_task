4ff5af689a976e3db8b22a3fd3edf16c0c40aee4
Savyata Bhattarai <bhattaraisavyata8@gmail.com>
2026-06-25 12:40:08 +0545

    Week8 Task-- complete docker setup and github actions pipeline

---
+++ app/pipeline.py
@@ -0,0 +1,17 @@
+import os
+def calculate_tax(amount:float,tax_rate:float)->float:
+    if amount < 0:
+        raise ValueError("Amount cannot be negative")
+    return round(amount * tax_rate , 2)
+def run_ingestion():
+    #fetcch configuration from environment variable
+    db_host = os.getenv("DB_HOST","localhost")
+    print(f"Connecting to data warehouse at {db_host}....")
+    #pipeline extraction logic goes here
+    print("Ingestion execution completed successfully")
+if __name__ =="__main__":
+    run_ingestion()
+
+
+    
+    
