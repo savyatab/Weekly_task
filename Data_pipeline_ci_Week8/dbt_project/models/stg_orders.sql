a8c80e7a50a78523ee205eb073e7173d0075423c
Savyata Bhattarai <bhattaraisavyata8@gmail.com>
2026-06-26 11:11:17 +0545

    Remove all tracked files to reset git

--- Data_pipeline_ci_Week8/dbt_project/models/stg_orders.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    id AS order_id,
-    customer_id,
-    status,
-    total_amount,
-    created_at
-FROM {{ source('raw_data', 'orders') }}
--- Data_pipeline_ci_Week8/dbt_project/tests/assert_valid_total.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    order_id,
-    total_amount
-FROM {{ ref('stg_orders') }}
-WHERE total_amount < 0
